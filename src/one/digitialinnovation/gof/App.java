3a38ad46261fea95c737794865c35c2d64cf623e
gsTavares <gustavo.tavares24@outlook.com>
2023-10-19 21:35:44 -0300

    first commit

---
+++ src/one/digitialinnovation/gof/App.java
@@ -0,0 +1,12 @@
+package one.digitialinnovation.gof;
+
+/**
+ * "Lazy" singleton
+ * 
+ * @author gstavares1
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
